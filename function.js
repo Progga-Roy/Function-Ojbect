8bf8f31ef3f0557b2a80deddd95cec841f00a88a
Progga <proggaroy2000@gmail.com>
2023-01-30 22:40:42 +0600

    function.js

---
+++ function.js
@@ -0,0 +1,32 @@
+function startFan(){
+console.log('Start the fan');
+console.log('Switch on');
+}
+// startFan();
+
+function bringSingara(money){
+console.log('Singara Price is');
+// console.log(money);
+}
+bringSingara(120);
+
+function chocolateKinbo(money,tk){
+
+    console.log("Chocolate er dam:", money, tk)
+}
+
+// chocolateKinbo(340,200)
+
+function add(num1,num2){
+    console.log('First and Second Number', num1,num2)
+}
+// add(12,9);
+
+
+function sum(a,b,c,d,e){
+    console.log(a,b,c,d,e);
+    var summation =a+b+c+d+e;
+    console.log( 'Summation =',summation)
+}
+
+sum(21,12,33,56,77)
